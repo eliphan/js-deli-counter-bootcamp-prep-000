6d37028d443a4a4cbda424702ec1a2a4ccdfe7b4
generic-thread-7687 <aliceconnic@gmail.com>
2018-10-15 01:18:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
 
 function takeANumber (line, name) {
-  var deliLine = []
-  
-  const l = deliLine.length
-  for (let i = 0; i < l; i++) {
-  
-  deliLine.push(`Welcome, ${name[i]}. You are number ${linelength[i]} in line`)
-  }
-  return deliLine
-}
+ 
