7d6664a9f2389d352667ac4770c1985ac57c9cd4
generic-thread-7687 <aliceconnic@gmail.com>
2018-10-15 01:22:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
 function takeANumber (line, name) {
  line.push(name)
- return `Welcome, ${name}. You are ${line.length} in line`
+ return `Welcome, ${name}. You are number ${line.length} in line`
  }
