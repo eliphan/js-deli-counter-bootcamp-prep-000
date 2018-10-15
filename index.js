87832e39c74fa425e6b803c09d9a938bb8b51da2
generic-thread-7687 <aliceconnic@gmail.com>
2018-10-15 01:16:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function takeANumber (line, name) {
   var deliLine = []
   
   const l = deliLine.length
-  
+  var i = 0
   for (i = 0; i < l; i++) {
   
   deliLine.push(`Welcome, ${name[i]}. You are number ${linelength[i]} in line`)
